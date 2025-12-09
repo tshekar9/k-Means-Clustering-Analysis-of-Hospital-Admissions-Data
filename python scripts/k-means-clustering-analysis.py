110cff984894a8796dffa392a6492e72beb0e86e
Tej <184202015+tshekar9@users.noreply.github.com>
2025-12-09 16:14:32 -0500

    Update k-means-clustering-analysis.py

--- python scripts/k-means-clustering-analysis.py
+++ python scripts/k-means-clustering-analysis.py
@@ -1,5 +1,12 @@
 # k-means clustering analysis
 
+#import required packages 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 
 # determine optimal number of clusters
